1b238a31949f1e447ac9cb13ba08088c906067d7
Daniel Hurny <P3502712@ness.com>
2017-07-04 14:59:03 +0200

    prepracovane vykreslenie

--- src/main/java/sk/tuke/gamestudio/game/pexeso/core/FieldPexeso.java
+++ src/main/java/sk/tuke/gamestudio/game/pexeso/core/FieldPexeso.java
@@ -1,5 +1,28 @@
 package sk.tuke.gamestudio.game.pexeso.core;
 
+import java.util.Random;
+
+import sk.tuke.gamestudio.game.kamene.core.GameState;
+
 public class FieldPexeso {
 
+	private int row;
+	private int column;
+	private Tile[][] tiles;
+	private GameState gameState = GameState.PLAYING;
+
+	public void generate() {
+		Random random = new Random();
+		for(int i=0;i<row*column;i++){
+		int isTwice = 0;
+		while (isTwice <= 1) {
+			int randomRow = random.nextInt(row);
+			int randomColumn = random.nextInt(column);
+			if (tiles[randomRow][randomColumn] == null) {
+				tiles[randomRow][randomColumn] = new Tile(i);
+				isTwice++;
+			}
+		}
+
+	}}
 }
--- src/main/java/sk/tuke/gamestudio/game/pexeso/core/Tile.java
+++ src/main/java/sk/tuke/gamestudio/game/pexeso/core/Tile.java
@@ -2,4 +2,42 @@ package sk.tuke.gamestudio.game.pexeso.core;
 
 public class Tile {
 
+	public enum State {
+		OPENED, CLOSED, CHOSEN
+	}
+	
+	private State state = State.CLOSED;
+	private Integer value;
+	
+	public Tile(int value) {
+		super();
+		this.value = value;
+	}
+	
+	public State getState() {
+		return state;
+	}
+	
+	public void setState(State state) {
+		this.state = state;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		if(state==State.CLOSED){
+			return "X";
+		}else return value.toString();
+		
+	}
+	
+	
+
 }
